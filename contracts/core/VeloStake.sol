365d3caef7da325543d0196c22f9cbba81e30af8
Jabr0nie <sammyllc19@gmail.com>
2025-05-10 16:03:59 -0500

    lets gooo

--- contracts/core/VeloStake.sol
+++ contracts/core/VeloStake.sol
@@ -3,7 +3,6 @@ pragma solidity =0.7.6;
 pragma abicoder v2;
 
 import "./interfaces/ICLPool.sol";
-
 import "./libraries/LowGasSafeMath.sol";
 import "./libraries/SafeCast.sol";
 import "./libraries/Tick.sol";
@@ -24,16 +23,66 @@ import "./interfaces/IERC20Minimal.sol";
 import "./interfaces/callback/ICLMintCallback.sol";
 import "./interfaces/callback/ICLSwapCallback.sol";
 import "contracts/libraries/VelodromeTimeLibrary.sol";
-import "./IERC721.sol";
 import "./INonfungiblePositionManager.sol";
+
+import {ERC721Holder} from "@openzeppelin/contracts/token/ERC721/ERC721Holder.sol";
 
 interface Oracle {
     function GetPrice(address pooladdress) external view returns (uint256 price);
     function getAmountsforLiquidity(address pool, uint256 usdAmount) external view returns (uint amount0, uint amount1);
 }
 
+interface IERC721 {
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+    function balanceOf(address owner) external view returns (uint256 balance);
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
+    function safeTransferFrom(address from, address to, uint256 tokenId) external;
+    function transferFrom(address from, address to, uint256 tokenId) external;
+    function approve(address to, uint256 tokenId) external;
+    function setApprovalForAll(address operator, bool approved) external;
+    function getApproved(uint256 tokenId) external view returns (address operator);
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
+}
 
-
+interface ICLGauge {
+    event NotifyReward(address indexed from, uint256 amount);
+    event Deposit(address indexed user, uint256 indexed tokenId, uint128 indexed liquidityToStake);
+    event Withdraw(address indexed user, uint256 indexed tokenId, uint128 indexed liquidityToStake);
+    event ClaimFees(address indexed from, uint256 claimed0, uint256 claimed1);
+    event ClaimRewards(address indexed from, uint256 amount);
+    function nft() external view returns (INonfungiblePositionManager);
+    function voter() external view returns (IVoter);
+    function pool() external view returns (ICLPool);
+    function feesVotingReward() external view returns (address);
+    function periodFinish() external view returns (uint256);
+    function rewardRate() external view returns (uint256);
+    function rewards(uint256 tokenId) external view returns (uint256);
+    function lastUpdateTime(uint256 tokenId) external view returns (uint256);
+    function rewardRateByEpoch(uint256) external view returns (uint256);
+    function fees0() external view returns (uint256);
+    function fees1() external view returns (uint256);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    function tickSpacing() external view returns (int24);
+    function left() external view returns (uint256 _left);
+    function rewardToken() external view returns (address);
+    function isPool() external view returns (bool);
+    function rewardGrowthInside(uint256 tokenId) external view returns (uint256);
+    function earned(address account, uint256 tokenId) external view returns (uint256);
+    function getReward(address account) external;
+    function getReward(uint256 tokenId) external;
+    function notifyRewardAmount(uint256 amount) external;
+    function notifyRewardWithoutClaim(uint256 amount) external;
+    function deposit(uint256 tokenId) external;
+    function withdraw(uint256 tokenId) external;
+    function stakedValues(address depositor) external view returns (uint256[] memory);
+    function stakedByIndex(address depositor, uint256 index) external view returns (uint256);
+    function stakedContains(address depositor, uint256 tokenId) external view returns (bool);
+    function stakedLength(address depositor) external view returns (uint256);
+}
 
 interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
@@ -48,7 +97,7 @@ interface IERC20 {
 }
 
 
-contract V3Bot {
+contract V3BotStaking is ERC721Holder{
     address public pool; // token0/token1 pool address
     address public token0; // token0 on Optimism
     address public token1; // token1 on Optimism
@@ -66,11 +115,10 @@ contract V3Bot {
         address token1;
     }
 
+    /// @dev deposits[tokenId] => Deposit
     mapping(uint256 => Deposit) public deposits;
 
-    event SwapExecuted(address indexed user, uint256 amountIn, uint256 amountOut);
-
-    constructor() {
+        constructor() {
         admin = msg.sender;
     }
 
@@ -79,18 +127,26 @@ contract V3Bot {
         admin = newAdmin;
     }
 
-    function _newSpaceMultiplier(int24 newSpaceMultiplier) external {
+    function _newtokenID(uint256 newTokenId) external {
         require(msg.sender == admin, "Only owner can do this");
-        spaceMultiplier = newSpaceMultiplier;
+        tokenId = newTokenId;
+    }
+// Us ICLGuage to stake the token
+//use the 
+    function stake() public payable {
+        IERC721(0x416b433906b1B72FA758e166e239c43d68dC6F29).transferFrom(msg.sender, address(this), tokenId);
+        IERC721(0x416b433906b1B72FA758e166e239c43d68dC6F29).approve(msg.sender, tokenId);
+        IERC721(0x416b433906b1B72FA758e166e239c43d68dC6F29).approve(0xC8c7b5aE61d97Be7d02d606629059487066DC9CF, tokenId);
+        ICLGauge(0xC8c7b5aE61d97Be7d02d606629059487066DC9CF).deposit(tokenId);
     }
 
-    function _newPool(address newPool) external {
-        require(msg.sender == admin, "Only owner can do this");
-        pool = newPool;
-        token0 = ICLPoolConstants(pool).token0();
-        token1 = ICLPoolConstants(pool).token1();
-        farmNFT = ICLPoolConstants(pool).nft();
-        tickSpacing = ICLPoolConstants(pool).tickSpacing();
+
+    function withdraw() public payable {
+        ICLGauge(0xC8c7b5aE61d97Be7d02d606629059487066DC9CF).withdraw(tokenId);
+    }
+
+    function sendNFTBack() public payable {
+        IERC721(0x416b433906b1B72FA758e166e239c43d68dC6F29).transferFrom(address(this), msg.sender, tokenId);
     }
 
     function _transferToAdmin(address Token) external {
@@ -98,60 +154,8 @@ contract V3Bot {
         IERC20Minimal(Token).transfer(admin, value);
     }
 
-    function getTicks () public view returns (int24 lowTick, int24 highTick){
-        (, int24 currentTick, , , , ) = ICLPool(pool).slot0();
-        currentTick = (currentTick / tickSpacing) * tickSpacing;
-        lowTick = currentTick - tickSpacing;
-        highTick = currentTick + tickSpacing;
-        return (lowTick, highTick);
-        }
+    
 
 
-function addLiquidity(uint256 amount0ToMint) public payable {
-    // Approve the position manager
-    (int24 tickLower,int24 tickUpper) = getTicks();
-
-    // Get square root prices for ticks
-    //(, int24 tick, , , , ) = ICLPoolState(pool).slot0();
-   //uint160 currentSqrtPrice = TickMath.getSqrtRatioAtTick(tick);
-    uint160 sqrtPriceLower = TickMath.getSqrtRatioAtTick(tickLower);
-    uint160 sqrtPriceUpper = TickMath.getSqrtRatioAtTick(tickUpper);
-
-
-
-    uint128 _liquidity = LiquidityAmounts.getLiquidityForAmount0(sqrtPriceLower, sqrtPriceUpper, amount0ToMint);
-
-    uint amount1ToMint =  LiquidityAmounts.getAmount1ForLiquidity(sqrtPriceLower, sqrtPriceUpper, _liquidity);
-
-    //(uint amount0ToMint, uint amount1ToMint) = Oracle(oracle).getAmountsforLiquidity(pool, _liquidity); -- V2 Pools
-
-    //Approve Tokens
-        IERC20(token0).transferFrom(msg.sender, address(this), amount0ToMint);
-        IERC20(token1).transferFrom(msg.sender, address(this), amount1ToMint);
-
-        IERC20(token0).approve(farmNFT, amount0ToMint);
-        IERC20(token1).approve(farmNFT, amount1ToMint);
-
-    // Get current sqrtPriceX96 from the pool
-
-        INonfungiblePositionManager.MintParams memory params =
-            INonfungiblePositionManager.MintParams({
-                token0: token0,
-                token1: token1,
-                tickSpacing: tickSpacing,
-                tickLower: tickLower, //add custom logic
-                tickUpper: tickUpper, //add custom logic
-                amount0Desired: amount0ToMint,
-                amount1Desired: amount1ToMint,
-                amount0Min: 0,
-                amount1Min: 0,
-                recipient: msg.sender,
-                deadline: block.timestamp + 1500000,
-                sqrtPriceX96: 0
-            });
-
-
-        (tokenId, , , ) = INonfungiblePositionManager(farmNFT).mint(params);
-}
 
 }
